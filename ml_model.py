85b4fcd9f4897bc90d352a188665b0c2565096a0
Byadab <binay3344@gmail.com>
2023-07-30 16:21:07 +0530

    adding new models functions to btranch

--- ml_model.py
+++ ml_model.py
@@ -2,3 +2,25 @@
 
 print('Addign all ml model for fraud detection')
 
+
+def model1():
+    print('adding ML model1')
+
+def model2():
+    print('adding ML model2')
+
+def model3():
+    print('adding ML model3')
+
+
+def model4():
+    print('adding ML model4')
+    
+
+
+
+
+
+
+
+
